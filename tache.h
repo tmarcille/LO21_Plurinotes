7bfe4914c9744b81089fde352bf2c0c7748ab528
Julien Crauser <jcrauser@hotmail.com>
2017-06-15 13:08:47 +0200

    commentaires tache

--- tache.h
+++ tache.h
@@ -11,32 +11,152 @@ class Tache : public Note {
 private:
     friend class NotesManager;
 
+    /**
+    * \brief    Action de la tache
+    */
+
     QString action;
+
+    /**
+    * \brief    Priorité de la tache
+    * \value    parmi {"Faible", "Moyenne", "Forte"}
+    */
+
     QString priorite;
+
+    /**
+    * \brief    Status de la tache
+    * \value    parmi {"en attente", "en cours", "terminee"}
+    */
+
     QString status;
+
+    /**
+    * \brief    La date Echeance de la tache
+    * \value    format QDate
+    */
+
     QDate echeance;
+
+    /**
+    * \brief    Echue, boolean qui permet savoir si l'on doit prendre en compte la date échéance
+    */
+
     bool echue;
+
+    /**
+    * \brief    Priorise, boolean qui permet savoir si l'on doit prendre en compte la priorite
+    */
+
     bool priorise;
+
     void saveAttributesInFile(QXmlStreamWriter& stream) const;
 
 protected :
     Tache(  const QString& f, const QString& ti, const QString& a="", const QDate &d=QDate(0,0,0), const QString &p="Faible", const QString &s="en attente", const bool &e = false, const bool &ps = false);
 
 public:
+
+    /**
+     * \brief    Retourne l'action de la tache
+     * \return   QString contenant l'action
+     */
+
     QString getAction() const {return action;}
+
+    /**
+     * \brief    Retourne la priorité de la tache
+     * \return   QString parmi {"Faible", "Moyenne", "Forte"}
+     */
+
     QString getPriorite() const {return priorite;}
-    QString getStatus() const {return status;}
+
+    /**
+     * \brief    Retourne le status de la tache
+     * \return   QString parmi {"en attente", "en cours", "terminee"}
+     */
+
+    QString getStatus() const {return status;} 
+
+    /**
+     * \brief    Retourne l'échéance de la tache
+     * \return   QDate que l'on convertira en string par la suite pour l'enregistrement dans le XML
+     */
+
     QDate getEcheance() const {return echeance;}
+
+    /**
+     * \brief    Retourne le boolean echue de la tache
+     * \return   QString parmis {"T","F"} pour faciliter l'insertion dans le XML
+     */
+
     QString getEchue() const;
+
+    /**
+     * \brief    Retourne le boolean priorise de la tache
+     * \return   QString parmis {"T","F"} pour faciliter l'insertion dans le XML
+     */
+
     QString getPriorise() const;
+
+    /**
+     * \brief    Retourne un nombre sous forme de Qstring pour faciliter le tri des taches en fonction de leur priorité
+     * \return   QString parmi
+     *          "1" -> "Forte"
+     *          "2" -> "Moyenne"
+     *          "3" -> "Faible"
+     *          "4" -> pas de priorité
+     */
+
     QString getIntPriorite() const;
+
+    /**
+     * \brief    Retourne le type de la note
+     * \return   QString contenant "tache"
+     */
+
     QString getType() const;
 
+    /**
+     * \brief    Permet de modifier l'action de la tache
+     * \param    t  QString contenant la nouvelle valeur de \a action
+     */
+
     void setAction(const QString& t);
+
+    /**
+     * \brief    Permet de modifier la priorité de la tache
+     * \param    p  QString contenant la nouvelle valeur de \a priorite
+     */
+
     void setPriorite(const QString& p);
+
+    /**
+     * \brief    Permet de modifier le status de la tache
+     * \param    s QString contenant la nouvelle valeur de \a status
+     */
+
     void setStatus(const QString& s);
-    void setEcheance(const QDate & echeance);
+
+    /**
+     * \brief    Permet de modifier le'cheance de la tache
+     * \param    d QString contenant la nouvelle valeur de \a echeance
+     */
+
+    void setEcheance(const QDate & d);
+
+    /**
+     * \brief    Permet de modifier echue de la tache
+     * \param    e QString contenant la nouvelle valeur de \a echue
+     */
+
     void setEchue(const QString & e);
+
+    /**
+     * \brief    Permet de modifier priorise de la tache
+     * \param    ps QString contenant la nouvelle valeur de \a echue
+     */
+
     void setPriorise(const QString &ps);
 };
 
--- tacheediteur.h
+++ tacheediteur.h
@@ -20,30 +20,145 @@ private:
 
     friend class NoteViewer;
 
+
+    /**
+     * \brief Layout local de l'editeur de tache
+     */
+
     QVBoxLayout * localLayout;
+
+    /**
+     * \brief H1 Layout local de l'editeur de tache attribué à la gestion de la priorité
+     *
+     */
+
     QHBoxLayout * h1;
+
+    /**
+     * \brief H2 Layout local de l'editeur de tache attribué à la gestion du status
+     *
+     */
+
     QHBoxLayout * h2;
+
+    /**
+     * \brief H3 Layout local de l'editeur de tache attribué à la gestion de l'echeance
+     *
+     */
+
     QHBoxLayout * h3;
+
+    /**
+     * \brief Label contenant "action"
+     *
+     */
+
     QLabel* actionl;
+
+    /**
+     * \brief Champ d'édition de l'action de la tache
+     *
+     */
+
     QTextEdit *action;
+
+    /**
+     * \brief Check Box permettant de modifier echue de la tache
+     *
+     */
+
     QCheckBox * echue;
+
+    /**
+     * \brief Label contenant "echeance"
+     *
+     */
+
     QLabel* echeancel;
+
+    /**
+     * \brief Champ d'édition de la date échéance de la tache
+     *
+     */
+
     QDateEdit *echeance;
+
+    /**
+     * \brief Check Box permettant de modifier priorise de la tache
+     *
+     */
+
     QCheckBox * priorise;
+
+    /**
+     * \brief Label contenant "priorité"
+     *
+     */
+
     QLabel* prioritel;
+
+    /**
+     * \brief Menu déroulant modifier la priorité parmi ("Fort", "Moyenne", "Faible") de la tache
+     *
+     */
+
     QComboBox *priorite;
+
+    /**
+     * \brief Label contenant "status"
+     *
+     */
+
     QLabel* statusl;
+
+    /**
+     * \brief boutton radio ayant pour valeur "en attente"
+     *
+     */
+
     QRadioButton *en_attente;
+
+    /**
+     * \brief boutton radio ayant pour valeur "en cours"
+     *
+     */
+
     QRadioButton *en_cours;
+
+    /**
+     * \brief boutton radio ayant pour valeur "terminee"
+     *
+     */
+
     QRadioButton *terminee;
+
+    /**
+     * \brief Regroupement des radios buttons pour modifier le status de la tache
+     *
+     */
+
     QButtonGroup *status;
+
 
     void sauvegardeAttributs();  
 
 protected :
+
+    /**
+    * \brief    Constructeur de l'editeur.
+    * \param    a  Tache* vers la tache qui sera affiché dans l'éditeur.
+    */
+
+
     TacheEditeur(Tache* a, QWidget *parent = 0);
 
 public:
+
+    /**
+    * \brief    Constructeur de l'editeur.
+    * \param    a Tache* vers la tache qui sera affiché dans l'éditeur.
+    */
+
     ~TacheEditeur();
 
 public slots :
